7faac94cb6ff431d6e83b4564a8935b9ee70cdd4
KageShiron <kageshiron@gmail.com>
2016-11-17 11:03:23 +0900

    move js to jsx

---
+++ jsx/content.jsx
@@ -0,0 +1,13 @@
+class TabTail extends React.Component {
+  render() {
+    return (
+    <li className="tab">
+    <div className="header">
+        <img className="favicon" src="https://www.google.co.jp/images/branding/product/ico/googleg_lodp.ico" />
+        <span className="title">Google XXXssssssssssssssssssssssssssの検索結果</span>
+    </div>
+    <img src="https://www.google.co.jp/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png" className="thumb" />
+    </li>
+    );
+  }
+} 

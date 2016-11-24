a7948570360fed4e5a0f5697174326068d8d2ccb
KageShiron <kageshiron@gmail.com>
2016-11-25 00:58:29 +0900

    Clean code by auto format

--- jsx/content.jsx
+++ jsx/content.jsx
@@ -1,78 +1,77 @@
-$( () => {
+$(() => {
 
-const React = require("react");
-const ReactDOM = require("react-dom");
-class TouchPadGesture{
-constructor(){
-this.keyStatus  = {
- ctrlKey : false,
- isShortCtrlKey : false,
- isTouchPad : false}
+  const React = require("react");
+  const ReactDOM = require("react-dom");
+  class TouchPadGesture {
+    constructor() {
+      this.keyStatus = {
+        ctrlKey: false,
+        isShortCtrlKey: false,
+        isTouchPad: false
+      }
 
-document.addEventListener( "keydown" ,(e) => {
-    console.log(`down/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
-  if(e.keyCode == 17 && !this.keyStatus.ctrlKey){
-    this.keyStatus.ctrlKey = true;
-    this.keyStatus.isShortCtrlKey = true;
-    setTimeout( () => {
-      this.keyStatus.isShortCtrlKey = false
-      console.log("shortTime");
-    } , 50);/*
+      document.addEventListener("keydown", (e) => {
+        console.log(`down/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
+        if (e.keyCode == 17 && !this.keyStatus.ctrlKey) {
+          this.keyStatus.ctrlKey = true;
+          this.keyStatus.isShortCtrlKey = true;
+          setTimeout(() => {
+            this.keyStatus.isShortCtrlKey = false
+            console.log("shortTime");
+          }, 50);/*
     setTimeout( () => {
       this.keyStatus.isShortCtrlKey = false;
       this.keyStatus.ctrlKey = false;
       this.keyStatus.isTouchPad = false;
     console.log("longTimer"); 
     } , 10000);*/
-  }
-});
-document.addEventListener( "keyup" ,(e) => {
-    console.log(`up/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
-  if(e.keyCode == 17)
-  {
-    this.keyStatus.ctrlKey = false;
-    this.keyStatus.isShortCtrlKey = false;
-    this.keyStatus.isTouchPad = false;
-  }
-}
-);
-window.addEventListener( "mousewheel",(e) => {
-  console.log(`wheel/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
-  if( this.keyStatus.isTouchPad ) //タッチパッド検出時は常にprevent
-  {
-    e.preventDefault();
-    return;
-  } 
-  if( e.ctrlKey && !this.keyStatus.ctrlKey) //時々keydownがこずにズームしようとする
-  {
-    e.preventDefault();
-    return;
-  }
-  if( !e.ctrlKey || !this.keyStatus.isShortCtrlKey)return; //no zoom || real ctrl key is pushed by user
-  console.log("touchpad!");
-  this.keyStatus.isTouchPad = true;
-  e.preventDefault();
-  if(jQuery("#arutab-insert-iframe").length != 0)return;
-  setTimeout(popupTablist,0);
-});
+        }
+      });
+      document.addEventListener("keyup", (e) => {
+        console.log(`up/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
+        if (e.keyCode == 17) {
+          this.keyStatus.ctrlKey = false;
+          this.keyStatus.isShortCtrlKey = false;
+          this.keyStatus.isTouchPad = false;
+        }
+      }
+      );
+      window.addEventListener("mousewheel", (e) => {
+        console.log(`wheel/${this.keyStatus.isTouchPad}/${e.ctrlKey}/${this.keyStatus.isShortCtrlKey}/${this.keyStatus.ctrlKey}`);
+        if (this.keyStatus.isTouchPad) //タッチパッド検出時は常にprevent
+        {
+          e.preventDefault();
+          return;
+        }
+        if (e.ctrlKey && !this.keyStatus.ctrlKey) //時々keydownがこずにズームしようとする
+        {
+          e.preventDefault();
+          return;
+        }
+        if (!e.ctrlKey || !this.keyStatus.isShortCtrlKey) return; //no zoom || real ctrl key is pushed by user
+        console.log("touchpad!");
+        this.keyStatus.isTouchPad = true;
+        e.preventDefault();
+        if (jQuery("#arutab-insert-iframe").length != 0) return;
+        setTimeout(popupTablist, 0);
+      });
 
-function popupTablist()
-{
-  console.log("popup");
-  jQuery("<iframe>").attr("id","arutab-insert-iframe").attr("src", chrome.runtime.getURL("tablist.html") )
-  .on("load",(e) => {
-    $(e.target).css("display","block") ;
-    setTimeout( () => $(e.target).addClass("arutab-iframe-visible"),0);
-    console.log($(e.target).attr("src"));
- }).css("display","none").appendTo("body");;
- setTimeout( () => jQuery("#arutab-insert-iframe").remove() , 10000);
-}
-}
-}
-new TouchPadGesture();
+      function popupTablist() {
+        console.log("popup");
+        jQuery("<iframe>").attr("id", "arutab-insert-iframe").attr("src", chrome.runtime.getURL("tablist.html"))
+          .on("load", (e) => {
+            $(e.target).css("display", "block");
+            setTimeout(() => $(e.target).addClass("arutab-iframe-visible"), 0);
+            console.log($(e.target).attr("src"));
+          }).css("display", "none").appendTo("body");;
+        setTimeout(() => jQuery("#arutab-insert-iframe").remove(), 10000);
+      }
+    }
+  }
+  new TouchPadGesture();
 
-chrome.runtime.onMessage.addListener((msg) =>{
-  if(msg.message == "closeAruTab")jQuery("#arutab-insert-iframe").remove();
-});
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.message == "closeAruTab") jQuery("#arutab-insert-iframe").remove();
+  });
 
 });
--- jsx/tablist.jsx
+++ jsx/tablist.jsx
@@ -1,99 +1,95 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-window.addEventListener("wheel",(e) => {
+window.addEventListener("wheel", (e) => {
     e.preventDefault()
 });
 
 class TabTail extends React.Component {
 
-constructor()
-{
-  super();
-  this.state = {
-    ctrlKey : false
-  }
+    constructor() {
+        super();
+        this.state = {
+            ctrlKey: false
+        }
+    }
+
+    render() {
+        const cname = "tab " + (this.props.actived == "actived" ? "actived" : "") + " size" + this.props.size;
+        return (
+            <li className={cname} onClick={ (e) => this.props.onClick(this.props.tabId) }>
+                <div className="header">
+                    <img className="favicon" src={ this.props.favicon } />
+                    <div className="title_url">
+                        <span className="title">{ this.props.title }</span><br />
+                        <span className="url">{ this.props.url }</span>
+                    </div>
+                </div>
+                <div className="thumbarea">
+                    <img src={ this.props.thumb } className="thumb" />
+                </div>
+            </li>
+        );
+    }
 }
 
-  render() {
-    const cname = "tab " + ( this.props.actived == "actived" ? "actived" : "") + " size" + this.props.size;
-    return (
-    <li className={cname} onClick={ (e) => this.props.onClick(this.props.tabId)}>
-        <div className="header">
-            <img className="favicon" src={ this.props.favicon } />
-            <div className="title_url">
-                <span className="title">{ this.props.title }</span><br />
-                <span className="url">{ this.props.url }</span>
-            </div>
-        </div>
-        <div className="thumbarea">
-            <img src={ this.props.thumb } className="thumb" />
-        </div>
-    </li>
-    );
-  }
-}
+class TabTailList extends React.Component {
 
-class TabTailList extends React.Component{
-
-    initConnection(){
-        this.port = chrome.runtime.connect(null,{});
-        this.port.onMessage.addListener( (arg) => {
-            switch(arg.message)
-            {
-                case "getTabs" :
-                for( const l of this.getTabsListeners )l(arg.tabs);
-                this.getTabsListeners = []; 
-                break;
+    initConnection() {
+        this.port = chrome.runtime.connect(null, {});
+        this.port.onMessage.addListener((arg) => {
+            switch (arg.message) {
+                case "getTabs":
+                    for (const l of this.getTabsListeners) l(arg.tabs);
+                    this.getTabsListeners = [];
+                    break;
             }
 
         });
         this.getTabsListeners = [];
     }
 
-    getTabs( callback ){
-        this.port.postMessage( { "message" : "getTabs" } );
+    getTabs(callback) {
+        this.port.postMessage({ "message": "getTabs" });
         this.getTabsListeners.push(callback);
     }
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            tabs : [],
-            captures : { }
+            tabs: [],
+            captures: {}
         }
         this.initConnection();
-        this.getTabs((tabs) =>
-        {
-         this.setState({tabs : tabs});
-         for( const t of tabs)
-         {
-             var def = {};
-             def[t.id] = "";
-             chrome.storage.local.get( def , (items) => {
-                var caps = this.state.captures;
-                caps[t.id] = items[t.id];
-                this.setState({captures : caps});
-            });
-         }
+        this.getTabs((tabs) => {
+            this.setState({ tabs: tabs });
+            for (const t of tabs) {
+                var def = {};
+                def[t.id] = "";
+                chrome.storage.local.get(def, (items) => {
+                    var caps = this.state.captures;
+                    caps[t.id] = items[t.id];
+                    this.setState({ captures: caps });
+                });
+            }
         }
-         );
+        );
     }
 
-    calcSize(){
-        if( this.state.tabs.length <= 9)return 30;
-        if( this.state.tabs.length <= 16)return 25;
+    calcSize() {
+        if (this.state.tabs.length <= 9) return 30;
+        if (this.state.tabs.length <= 16) return 25;
         return 20;
     }
 
     render() {
         console.log(this.state);
-        const list = this.state.tabs.map( (t) => <TabTail key={t.id} tabId={t.id} onClick={(e) => this.handleTabClick(e)} title={t.title} url={t.url} favicon={t.favIconUrl || "chrome://favicon/" + t.url} thumb={ this.state.captures[t.id] } actived={t.active ? "actived" : ""} size={this.calcSize()} />)
+        const list = this.state.tabs.map((t) => <TabTail key={t.id} tabId={t.id} onClick={(e) => this.handleTabClick(e) } title={t.title} url={t.url} favicon={t.favIconUrl || "chrome://favicon/" + t.url} thumb={ this.state.captures[t.id]} actived={t.active ? "actived" : ""} size={this.calcSize() } />)
         return <ul>{list}</ul>
     }
 
-    handleTabClick(tabId){
-        this.port.postMessage( {message:"requestTabChange",tabId:tabId});
+    handleTabClick(tabId) {
+        this.port.postMessage({ message: "requestTabChange", tabId: tabId });
     }
 }
 
